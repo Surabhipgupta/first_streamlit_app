343bf7f87f7b504693dcef9c6819af4af4459264
Surabhipgupta <151066847+Surabhipgupta@users.noreply.github.com>
2023-12-01 00:23:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,22 +44,33 @@ except URLError as e:
 streamlit.stop()
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 #my_data_row = my_cur.fetchone()
 #streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_row)
 
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#c
 #my_cur = my_cnx.cursor()
 my_cur.execute("Use warehouse PC_RIVERY_WH")
 my_cur.execute("Use ROLE ACCOUNTADMIN")
 my_cur.execute("Use schema PC_RIVERY_DB.PUBLIC")
-my_cur.execute("select * from fruit_load_list")
+#my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Snowflake related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+    
 #streamlit.text(my_data_rows)
 
 
