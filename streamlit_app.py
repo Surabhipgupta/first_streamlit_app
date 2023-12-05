19b5947033514fe072e03a1ac4154de6ba8080a2
Surabhipgupta <151066847+Surabhipgupta@users.noreply.github.com>
2023-12-05 08:54:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,14 +53,17 @@ except URLError as e:
 
 #c
 
-my_cur.execute("Use warehouse PC_RIVERY_WH")
-my_cur.execute("Use ROLE ACCOUNTADMIN")
-my_cur.execute("Use schema PC_RIVERY_DB.PUBLIC")
+#my_cur.execute("Use warehouse PC_RIVERY_WH")
+#my_cur.execute("Use ROLE ACCOUNTADMIN")
+#my_cur.execute("Use schema PC_RIVERY_DB.PUBLIC")
 #my_cur.execute("select * from fruit_load_list")
 streamlit.header("The fruit loadlist contains:")
 #Snowflake related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
+        my_cur.execute("Use warehouse PC_RIVERY_WH")
+        my_cur.execute("Use ROLE ACCOUNTADMIN")
+        my_cur.execute("Use schema PC_RIVERY_DB.PUBLIC")
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 
